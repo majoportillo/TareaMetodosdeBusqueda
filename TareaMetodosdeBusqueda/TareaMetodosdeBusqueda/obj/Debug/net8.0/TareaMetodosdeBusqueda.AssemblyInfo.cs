fbb94a547ba48a8f4e84c51a6293a36e78058e30
majito <mariajoseportill2@gmail.com>
2025-02-15 21:51:38 -0600

    commit 2

--- TareaMetodosdeBusqueda/TareaMetodosdeBusqueda/obj/Debug/net8.0/TareaMetodosdeBusqueda.AssemblyInfo.cs
+++ TareaMetodosdeBusqueda/TareaMetodosdeBusqueda/obj/Debug/net8.0/TareaMetodosdeBusqueda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareaMetodosdeBusqueda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ba29489ee49ff52c58ed5cf0f6bfd23cc357ce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareaMetodosdeBusqueda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareaMetodosdeBusqueda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
